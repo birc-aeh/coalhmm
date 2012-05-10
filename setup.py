85b25d7d7354a5d6763ca527132feb352baa6f1b
Anders Halager <aeh@birc.au.dk>
2012-05-10 12:04:17 +0200

    Added distutils script

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='CoalHMM',
+      version='0.0',
+      author='Anders Halager',
+      author_email='aeh@birc.au.dk',
+      packages=['coalhmm'],
+      package_dir = {'coalhmm': 'coalhmm'}
+     ) 
+
