9d92e02abf8f41f4f69a2d0e2c14699b62549713
Thomas Mailund <mailund@birc.au.dk>
2013-06-14 16:13:44 +0200

    Updated analysis script to include calculation of AIC

--- test_data/analysis.sh
+++ test_data/analysis.sh
@@ -32,14 +32,57 @@ done
 echo "Done."
 echo
 
-echo "Analysis I data with I model."
+echo
+echo "ANALYSIS OF I DATA"
+echo
+
+echo "Analyse I data with I model."
 cmd="python ../scripts/estimate-using-I-model.py --verbose --header --out=I-results-I.txt analysis/sim_I_*"
 echo $cmd
 $cmd
 echo
 
-echo "Analysis I data with IM model."
+echo "Analyse I data with IM model."
 cmd="python ../scripts/estimate-using-IM-model.py --verbose --header --out=I-results-IM.txt analysis/sim_I_*"
 echo $cmd
 $cmd
+echo
+
+
+echo
+echo "ANALYSIS OF IM DATA"
+echo
+
+echo "Analyse IM data with I model."
+cmd="python ../scripts/estimate-using-I-model.py --verbose --header --out=IM-results-I.txt analysis/sim_IM_*"
+echo $cmd
+$cmd
+echo
+
+echo "Analyse IM data with IM model."
+cmd="python ../scripts/estimate-using-IM-model.py --verbose --header --out=IM-results-IM.txt analysis/sim_IM_*"
+echo $cmd
+$cmd
+echo
+
+echo
+echo "Model checking for isolation model:"
+IlogL=`tail -n 1 I-results-I.txt | cut -f1`
+IMlogL=`tail -n 1 I-results-IM.txt | cut -f1`
+IAIC=`bc -q <( echo "2 * (3 - $IlogL)"  ; echo quit )`
+IMAIC=`bc -q <( echo "2 * (5 - $IMlogL)"  ; echo quit )`
+Delta=`bc -q <( echo $IlogL - $IMlogL ; echo quit )`
+
+echo "I AIC = $IAIC, IM AIC = $IMAIC, DeltaAIC = $Delta"
+echo
+
+echo "Model checking for migration model:"
+
+IlogL=`tail -n 1 IM-results-I.txt | cut -f1`
+IMlogL=`tail -n 1 IM-results-IM.txt | cut -f1`
+IAIC=`bc -q <( echo "2 * (3 - $IlogL)"  ; echo quit )`
+IMAIC=`bc -q <( echo "2 * (5 - $IMlogL)"  ; echo quit )`
+Delta=`bc -q <( echo $IlogL - $IMlogL ; echo quit )`
+
+echo "I AIC = $IAIC, IM AIC = $IMAIC, DeltaAIC = $Delta"
 echo
