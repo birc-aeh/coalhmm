7c0843f5f9b132bb813aae923f74080344e92c84
Thomas Mailund <mailund@birc.au.dk>
2013-07-14 19:23:42 +0200

    A bit more posterior analysis...

--- test_data/estimating-C.R
+++ test_data/estimating-C.R
@@ -11,6 +11,19 @@ posteriors <- cbind(read.table('marginal-posterior-0.txt',header=TRUE),
                     read.table('marginal-posterior-9.txt',header=TRUE)[,2])
 names(posteriors) <- c('break.times',paste('posterior.',1:10,sep=''))
 
+posteriors <- cbind(read.table('marginal-posterior-20states-0.txt',header=TRUE),
+                    read.table('marginal-posterior-20states-1.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-2.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-3.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-4.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-5.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-6.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-7.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-8.txt',header=TRUE)[,2],
+                    read.table('marginal-posterior-20states-9.txt',header=TRUE)[,2])
+names(posteriors) <- c('break.times',paste('posterior.',1:10,sep=''))
+
+
 break.times <- posteriors$break.times
 posteriors <- posteriors[,2:11]
 
@@ -30,23 +43,49 @@ bootstraps <- replicate(n=10, {
   estimate.Cs(apply(boot.posteriors,1,mean))
 })
 
+opar <- par(mfrow=c(2,1))
+par(mar=c(1, 4, 2, 2) + 0.1)
 plot(range(break.times), range(total.Cs), type='n', ylim=c(400,600),
-     main='Estimation of coalescence rates over time',
+     #main='Estimation for individual chunks',
+     xlab='Time (mutations per basepair)', ylab='Coalescence rate (C)', xaxt='n')
+abline(v=break.times, lty='dashed',col='gray')
+for (i in 1:ncol(posteriors)) {
+  segments(x0 = break.times[1:(nstates-1)], x1 = break.times[2:nstates], 
+           y0 = estimate.Cs(posteriors[,i]), col='darkgreen')  
+}
+abline(h=500, col='red')
+
+par(mar=c(2, 4, 1, 2) + 0.1)
+plot(range(break.times), range(total.Cs), type='n', ylim=c(400,600),
+     #main='Estimation for combined chunks',
      xlab='Time (mutations per basepair)', ylab='Coalescence rate (C)')
 abline(v=break.times, lty='dashed',col='gray')
 for (i in 1:ncol(bootstraps)) {
   segments(x0 = break.times[1:(nstates-1)], x1 = break.times[2:nstates], 
-           y0 = as.vector(bootstraps[,i]), col='gray')  
+           y0 = bootstraps[,i], col='gray')  
 }
 segments(x0 = break.times[1:(nstates-1)], x1 = break.times[2:nstates], 
          y0 = total.Cs, col='blue', lwd=2)
 abline(h=500, col='red')
+par(opar)
 
-plot(range(c(break.times,1.5*break.times[nstates])), range(total.post), type='n', ylim=c(0.05,0.15),
-     main='Posterior probabilities',
+opar <- par(mfrow=c(2,1))
+par(mar=c(1, 4, 2, 2) + 0.1)
+plot(range(c(break.times,1.5*break.times[nstates])), range(posteriors), type='n',
+     #     main='Posterior probabilities',
+     xaxt='n',
+     xlab='Time (mutations per basepair)', ylab='Posterior')
+abline(v=break.times, lty='dashed',col='gray')
+for (i in 1:ncol(posteriors)) {
+  segments(x0 = break.times[1:(nstates)], x1 = c(break.times[2:nstates],1.5*break.times[nstates]), 
+           y0 = posteriors[,i], col='darkgreen')  
+}
+
+par(mar=c(2, 4, 1, 2) + 0.1)
+plot(range(c(break.times,1.5*break.times[nstates])), range(total.post), type='n',
+#     main='Posterior probabilities',
      xlab='Time (mutations per basepair)', ylab='Posterior')
 abline(v=break.times, lty='dashed',col='gray')
 segments(x0 = break.times[1:(nstates)], x1 = c(break.times[2:nstates],1.5*break.times[nstates]), 
          y0 = total.post, col='blue', lwd=2)
-abline(h=0.1, col='red')
-
+par(opar)
--- test_data/posterior-analysis.sh
+++ test_data/posterior-analysis.sh
@@ -2,7 +2,11 @@
 
 for chunk in {0..9};
 do
-    python ../scripts/posterior-from-I-model.py -T 0.004 -C 500 -R 0.2 analysis/sim_I_${chunk}.ziphmm | \
-    tee posterior-${chunk}.txt | \
+    python ../scripts/posterior-from-I-model.py -T 0.004 -C 497 -R 0.2 analysis/sim_I_${chunk}.ziphmm | \
     python ../scripts/marginal-posterior.py > marginal-posterior-${chunk}.txt
+
+    python ../scripts/posterior-from-I-model.py -T 0.004 -C 497 -R 0.2 --intervals=20 analysis/sim_I_${chunk}.ziphmm | \
+    python ../scripts/marginal-posterior.py > marginal-posterior-20states-${chunk}.txt
+
+#    tee posterior-${chunk}.txt | \
 done
